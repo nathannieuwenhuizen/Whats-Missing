e87364d2a2ad300d3a61a392e8d26ac6b7e6e09c
Nathan Nieuwenhuizen <nathan-san@live.nl>
2021-03-20 14:17:06 +0100

    added sub colors to be missing

---
+++ Assets/Scripts/inGameObjects/Properties/BlueProperty.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class BlueProperty : Property
+{
+    [SerializeField]
+    private Room room;
+
+    public override void onMissing()
+    {
+        base.onMissing();
+        redValue = 0;
+    }
+    public override void onAppearing()
+    {
+        base.onAppearing();
+        redValue = 100f;
+
+    }
+
+    public float redValue {
+        set {
+            room.Player.Camera.gameObject.gameObject.GetComponent<PostProcessVolume>().profile.GetSetting<ColorGrading>().mixerBlueOutBlueIn.value = value;
+        }
+    }
+}
---
+++ Assets/Scripts/inGameObjects/Properties/GreenProperty.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class GreenProperty : Property
+{
+    [SerializeField]
+    private Room room;
+
+    public override void onMissing()
+    {
+        base.onMissing();
+        greenValue = 0;
+    }
+    public override void onAppearing()
+    {
+        base.onAppearing();
+        greenValue = 100f;
+
+    }
+
+    public float greenValue {
+        set {
+            // room.Player.Camera.gameObject.gameObject.GetComponent<PostProcessVolume>().profile.GetSetting<ColorGrading>().mixerRedOutGreenIn.value = value;
+            room.Player.Camera.gameObject.gameObject.GetComponent<PostProcessVolume>().profile.GetSetting<ColorGrading>().mixerGreenOutGreenIn.value = value;
+        }
+    }
+}
