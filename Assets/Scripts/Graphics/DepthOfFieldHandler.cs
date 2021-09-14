8ccbec3819be67cdbe74a9e8970f265c0bcc8a0f
Nathan Nieuwenhuizen <nathan-san@live.nl>
2021-09-14 14:30:22 +0200

    depth of field added

--- Assets/Scripts/Graphics/DepthOfFieldHandler.cs
+++ Assets/Scripts/Graphics/DepthOfFieldHandler.cs
@@ -5,17 +5,46 @@ using UnityEngine.Rendering.PostProcessing;
 public class DepthOfFieldHandler : MonoBehaviour
 {
 
-    private Ray rayCast;
-    
-    // Start is called before the first frame update
-    void Start()
-    {        
-        GameObject go;
+    private Ray ray;
+    private RaycastHit hit;
+
+    private PostProcessVolume volume;
+    private DepthOfField depthOfField;
+
+    private bool isHit = false;
+    private float hitDistance;
+
+    private float lerpSpeed = 50f;
+
+    private void Start() {
+        volume = GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings<DepthOfField>(out depthOfField);
+    }
+    private void Update() {
+        ray = new Ray(transform.position, transform.forward * 100);
+        if (Physics.Raycast(ray, out hit, 100f))
+        {    
+            isHit = true;
+            hitDistance = hit.distance;
+            Debug.Log("hit!: " + hit.distance);
+        } else {
+            if (hitDistance < 100f) {
+                hitDistance++;
+            }
+        }
+        SetFocus();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void SetFocus() {
+        depthOfField.focusDistance.value = Mathf.Lerp(depthOfField.focusDistance.value, hitDistance, Time.unscaledDeltaTime * hitDistance);
+    }
+
+    private void OnDrawGizmos() {
+        if (isHit) {
+            Gizmos.DrawSphere(hit.point, 0.1f);
+            Debug.DrawRay(transform.position, transform.forward * Vector3.Distance(transform.position, hit.point));
+        } else {
+            Debug.DrawRay(transform.position, transform.forward * 100f);
+        }
     }
 }
