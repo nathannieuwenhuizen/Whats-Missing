3c6081364da44f5638b2b930baca04a94c32f10c
Nathan Nieuwenhuizen <nathan-san@live.nl>
2021-11-17 16:27:12 +0100

    chair disabled to move

--- Assets/Scripts/inGameObjects/UniversalObjects/Chair.cs
+++ Assets/Scripts/inGameObjects/UniversalObjects/Chair.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chair : PickableRoomObject
+public class Chair : RoomObject
 {
     private void OnCollisionEnter(Collision other) {
-        if (rb == null) return;
-        AudioHandler.Instance.Player3DSound(SFXFiles.chair_hit, transform, .2f * Mathf.Min(1, RigidBody.velocity.magnitude / 5f),1f, false, true, 30);
+        // if (rb == null) return;
+        // AudioHandler.Instance.Player3DSound(SFXFiles.chair_hit, transform, .2f * Mathf.Min(1, RigidBody.velocity.magnitude / 5f),1f, false, true, 30);
     }
 }
