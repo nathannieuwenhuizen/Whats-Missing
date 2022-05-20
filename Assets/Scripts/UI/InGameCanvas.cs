e2f9553d0b27898fab443a14fb74d3df00102971
Nathan Nieuwenhuizen <nathan-san@live.nl>
2022-05-20 12:51:48 +0200

    canvas toggle

---
+++ Assets/Scripts/UI/InGameCanvas.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameCanvas : MonoBehaviour
+{
+    private CanvasGroup cg;
+
+    private void Awake() {
+        cg = GetComponent<CanvasGroup>();
+    }
+
+    private void Update() {
+        #if UNITY_EDITOR
+
+        if (Input.GetKeyDown(KeyCode.C))  
+            cg.alpha = cg.alpha == 1 ? 0 : 1;
+            #endif
+        
+    }
+}
